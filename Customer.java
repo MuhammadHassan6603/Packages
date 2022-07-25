62acfb0fb224281126362bdb8ff774260425c29c
Muhammad Hassan <105521572+MuhammadHassan6603@users.noreply.github.com>
2022-07-26 02:25:37 +0500

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,9 @@
+package PKG;
+public class Customer{
+    public String name;
+    public int salary;
+    public void Show(String a,int b){
+        name=a;
+        salary=b;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+import PKG.Customer;
+public class Main{
+public static void main(String args[]){
+    Customer obj=new Customer();
+    obj.Show("Hassan",25000);
+    System.out.println(obj.salary);
+    System.out.println(obj.name);
+}
+}
